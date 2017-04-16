b9bd3af9975c73c3f90e0a6dec7ab2299bccf17c
Kyle Holzinger <kylelholzinger@gmail.com>
2017-04-16 01:24:47 -0400

    [KH] respond with json

---
+++ src/main.ml
@@ -0,0 +1,34 @@
+open Opium.Std
+
+type person = {
+  name: string;
+  age: int;
+}
+
+let json_of_person { name ; age } =
+  let open Ezjsonm in
+  dict [ "name", (string name)
+       ; "age", (int age) ]
+
+let print_param = get "/hello/:name" begin fun req ->
+    `String ("Hello " ^ param req "name") |> respond'
+  end
+
+let print_person = get "/person/:name/:age" begin fun req ->
+    let person = {
+      name = param req "name";
+      age = "age" |> param req |> int_of_string;
+    } in
+    `Json (person |> json_of_person) |> respond'
+  end
+
+let deal_term_calculation = post "/deal_term_calculations" begin fun req ->
+    App.json_of_body_exn req |> Lwt.map (fun _json -> respond (`Json _json))
+  end
+
+let _ =
+  App.empty
+  |> print_param
+  |> print_person
+  |> deal_term_calculation
+  |> App.run_command
